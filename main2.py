1cf600d0d39ee8eac35bdd9005c723c68963cb3c
ThaRising <kochbe.ber@gmail.com>
2019-12-11 23:02:08 +0100

    Added Download functionality to program

--- main2.py
+++ main2.py
@@ -8,6 +8,8 @@ from threading import Thread
 from queue import Queue
 from dotenv import load_dotenv
 import requests
+from requests_futures.sessions import FuturesSession
+import wget
 
 load_dotenv()
 
@@ -26,16 +28,16 @@ class PrepWorker():
         await captcha.screenshot({'path': 'captcha.png'})
         return page
 
-    async def dispatcher(self):
+    @staticmethod
+    async def dispatcher():
         browser = await launch({"headless": False})
         page = await PrepWorker.__prep(browser)
         return browser, page
 
 
 class AuthWorker(Thread):
-    def __init__(self, queue, page):
+    def __init__(self, page):
         Thread.__init__(self)
-        self.queue = queue
         self.page = page
         self.username = os.getenv("USR")
         self.password = os.getenv("PWD")
@@ -55,39 +57,71 @@ class AuthWorker(Thread):
         return self.page
 
     def run(self):
-        nPage = asyncio.get_event_loop().run_until_complete(self.login())
-        self.queue.put(nPage)
+        asyncio.get_event_loop().run_until_complete(self.login())
 
 
-class QueueCrawler(Thread):
-    def __init__(self, queue, page):
+class IdScraper(Thread):
+    def __init__(self, browser, page):
         Thread.__init__(self)
+        self.browser = browser
+        self.page = page
 
     def run(self):
-        pass
+        asyncio.get_event_loop().run_until_complete(self.log())
+        return 0
 
     async def log(self):
-        async with open ("logfile.txt", "w+") as fin:
-            async for lines in fin:
-                pass
+        ua = await self.browser.userAgent()
+        cookies = await self.page.cookies()
+        if not os.path.isfile("logfile.txt"):
+            with open("logfile.txt", "w+") as fout:
+                data = requests.get("https://pr0gramm.com/api/items/get",
+                                    params={"flags": "9", "likes": os.getenv("USR"), "self": "true"},
+                                    headers={"accept": "application/json",
+                                             "user-agent": ua, "referer": os.getenv("MAIN_PAGE")},
+                                    cookies={cookies[-1].get("name"): cookies[-1].get("value")})
+                if not data.status_code == requests.codes.ok:
+                    raise Exception('Request rejected by server, please restart the program and try again.')
+                ids = ["{}:{}".format(n["id"], n["image"]) for n in data.json()["items"]]
+                for item in ids:
+                    fout.write("{}{}".format(item, "\n"))
+                try:
+                    while len(data.json()["items"]) > 0:
+                        with FuturesSession(max_workers=4) as session:
+                            data = session.get("https://pr0gramm.com/api/items/get",
+                                               params={"older": str(int(ids[-1].split(":")[0]) - 120), "flags": "9",
+                                                       "likes": os.getenv("USR"), "self": "true"},
+                                               headers={"accept": "application/json", "user-agent": ua,
+                                                        "referer": os.getenv("MAIN_PAGE")},
+                                               cookies={cookies[-1].get("name"): cookies[-1].get("value")})
+                            data = data.result()
+                            ids = ["{}:{}".format(n["id"], n["image"]) for n in data.json()["items"]]
+                            for item in ids:
+                                fout.write("{}{}".format(item, "\n"))
+                finally:
+                    return 0
 
 
-class DonwloadWorker(Thread):
-    def __init__(self, queue):
+class DownloadWorker(Thread):
+    def __init__(self, browser, page):
         Thread.__init__(self)
+        self.browser = browser
+        self.page = page
 
-    def run(self):
-        pass
+    def run(self) -> None:
+        asyncio.get_event_loop().run_until_complete(self.reqImg())
 
-    def download(self):
-        pass
+    async def reqImg(self):
+        with open("logfile.txt", "r") as fin:
+            img = wget.download(
+                "{}/{}".format("https://img.pr0gramm.com", str(fin.readline().split(":")[1].strip())))
 
 
 class Gui():
     def __init__(self):
         self.authQueue = Queue()
         print("Dispatching PrepWorker() - Thread: NULL")
-        self.browser, self.page = asyncio.get_event_loop().run_until_complete(PrepWorker().dispatcher())
+        self.browser, self.page = asyncio.get_event_loop().run_until_complete(PrepWorker.dispatcher())
         print("Dispatching AuthWorker() - Thread: 1")
         AuthWorker(self.authQueue, self.page).run()
         while True:
@@ -97,27 +131,8 @@ class Gui():
                 break
             except Queue.Empty:
                 continue
-        asyncio.get_event_loop().run_until_complete(self.sc())
-
-    async def sc(self):
-        ua = await self.browser.userAgent()
-        cookies = await self.page.cookies()
-        cookies = cookies[-1]
-        stream = requests.get("https://pr0gramm.com/api/items/get",
-                              params={"older": "3525673", "flags": "9", "likes": os.getenv("USR"), "self": "true"},
-                              headers={"accept": "application/json",
-                                       "user-agent": ua, "referer": os.getenv("MAIN_PAGE")},
-                              cookies={cookies.get("name"): cookies.get("value")})
-        if not stream.status_code == requests.codes.ok:
-            raise Exception('Request rejected by server, please restart the program and try again.')
-        ids = [n["id"] for n in stream.json()["items"]]
-        with open("logfile.txt","w+") as fout:
-            for items in ids:
-                fout.write("{}{}".format(items, "\n"))
-        await self.page.evaluate("""{window.scrollBy(0, document.body.scrollHeight);}""")
-        await asyncio.sleep(10)
-        imgStream = await self.page.waitForXPath('//*[@id="stream"]')
-        await imgStream.screenshot({'path': 'page.png'})
+        IdScraper(self.browser, self.page).run()
+        DownloadWorker(self.browser, self.page).run()
 
 
 if __name__=="__main__":
