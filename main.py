c182fef1197cb618f5ff92d4959a3f0603951a04
ThaRising <kochbe.ber@gmail.com>
2020-02-09 18:09:35 +0100

    Improved startup performance

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # !python3
 # -*- coding: utf-8 -*-
 
-from multiprocessing.queues import Queue
-from multiprocessing.spawn import freeze_support
-import services
+import multiprocessing
 import processes
+from Browser import Browser
 import Gui
 
 if __name__ == "__main__":
-    freeze_support()
-    tasks = Queue()
+    multiprocessing.freeze_support()
+    tasks = multiprocessing.Queue()
     threads = [processes.DownloadWorker(tasks), processes.DownloadWorker(tasks)]
     for thread in threads:
         thread.daemon = True
         thread.start()
-    Gui.App(tasks).mainloop()
+    browser = Browser()
+    Gui.Gui(browser, tasks).mainloop()
